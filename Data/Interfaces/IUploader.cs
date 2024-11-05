ba97efc63b12d0ffd4894e66776d026ffc199136
Yogeshdesai0817 <65127711+Yogeshdesai0817@users.noreply.github.com>
2024-11-05 19:20:09 +0100

    Test function

--- Data/Interfaces/IUploader.cs
+++ Data/Interfaces/IUploader.cs
@@ -1,5 +1,8 @@
 ﻿namespace Data.Interfaces
 {
+    /// <summary>
+    /// Test
+    /// </summary>
     public interface IUploader
     {
         Task<bool> UploadDataAsync(string filename, string data);
